b20bf9ac4c4352be35cdb0210c23d0c0e4f709da
Adrián Ortega <adrianog.sw@gmail.com>
2024-01-14 14:24:23 -0600

    Section 5: conditional rendering with &&

--- 03-pizza-menu-adrian/src/index.js
+++ 03-pizza-menu-adrian/src/index.js
@@ -68,14 +68,18 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
@@ -95,8 +99,8 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
-  const closeHour = 20;
+  const openHour = 12;
+  const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   console.log(hour);
@@ -106,7 +110,12 @@ function Footer() {
   //   return React.createElement("footer", null, "We're currently open!");
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order now</button>
+        </div>
+      )}
     </footer>
   );
 }
