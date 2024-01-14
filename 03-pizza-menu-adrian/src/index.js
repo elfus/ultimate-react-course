56b2dd788caa9285f23dedd4309478cdc60e369a
Adrián Ortega <adrianog.sw@gmail.com>
2024-01-14 14:51:46 -0600

    Section 5: Conditional rendering with ternaries

--- 03-pizza-menu-adrian/src/index.js
+++ 03-pizza-menu-adrian/src/index.js
@@ -73,12 +73,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -110,11 +112,15 @@ function Footer() {
   //   return React.createElement("footer", null, "We're currently open!");
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order now</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
