89925b00ccbd9696f9674505f638ccc1a0b56464
Adrián Ortega <adrianog.sw@gmail.com>
2024-01-14 15:20:25 -0600

    Section 5: React Fragments

--- 03-pizza-menu-adrian/src/index.js
+++ 03-pizza-menu-adrian/src/index.js
@@ -69,16 +69,25 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        // Simple fragment can be declared with <></>
+        <React.Fragment key="asd">
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
