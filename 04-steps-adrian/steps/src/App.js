bc4621b1710a6fa58e24cda3c3e69d6e6a8f9ca0
Adrián Ortega <adrianog.sw@gmail.com>
2024-01-16 10:34:53 -0600

    Section 6: Don´t set state manually!

--- 04-steps-adrian/steps/src/App.js
+++ 04-steps-adrian/steps/src/App.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "Jonas" });
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -15,6 +16,11 @@ export default function App() {
 
   function handleNext() {
     if (step < 3) setStep(step + 1);
+    //BAD PRACTICE
+    // test.name = "Fred";
+
+    //ALWAYS USE set* functions to state
+    setTest({ name: "Fred" });
   }
 
   return (
@@ -25,7 +31,7 @@ export default function App() {
         <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
-        Step {step}: {messages[step - 1]}
+        Step {step}: {messages[step - 1]} {test.name}
       </p>
       <div className="buttons">
         <button
