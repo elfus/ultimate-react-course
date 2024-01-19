3a9abaa964f1fb06ab0d3f816a4b7ebe112791f3
Adrián Ortega <adrianog.sw@gmail.com>
2024-01-19 10:31:30 -0600

    Section 7: More reusability with the "children" prop

--- 04-steps-adrian/steps/src/App.js
+++ 04-steps-adrian/steps/src/App.js
@@ -46,9 +46,20 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Leartn how to ${messages[step - 1]}`)}
+              >
+                Learn how to{" "}
+              </Button>
+            </div>
+          </StepMessage>
+
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               <span>👈</span>Previous
@@ -60,6 +71,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3>
+      {children}
     </div>
   );
 }
