46a20016b5a9203c16e145cdc837827d0cdc960d
Adrián Ortega <adrianog.sw@gmail.com>
2024-02-11 15:53:57 -0600

    Section 12: Effects and data fetching
    Video 149. Synchronizing queries with movie data

--- 07-usepopcorn-adrian/usepopcorn/src/index.js
+++ 07-usepopcorn-adrian/usepopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 function Test() {
@@ -17,14 +17,14 @@ function Test() {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    />
-    <StarRating size={24} color="red" />
-    <Test />
-  </React.StrictMode>
+  <App />
+  // <React.StrictMode>
+  //   <StarRating
+  //     maxRating={5}
+  //     messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+  //     defaultRating={3}
+  //   />
+  //   <StarRating size={24} color="red" />
+  //   <Test />
+  // </React.StrictMode>
 );
