1f250b368ef81fdec382488dd481a405bcec577b
Adrián Ortega <adrianog.sw@gmail.com>
2024-02-13 12:33:28 -0600

    Section 12: Effects and Data Fetching
    Video 155: Cleaning up data fetching

--- 07-usepopcorn-adrian/usepopcorn/src/App.js
+++ 07-usepopcorn-adrian/usepopcorn/src/App.js
@@ -97,12 +97,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
@@ -113,7 +116,8 @@ export default function App() {
           setMovies(data.Search);
         } catch (err) {
           console.log(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") setError(err.message);
+          setError("");
         } finally {
           setIsLoading(false);
         }
@@ -124,6 +128,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
