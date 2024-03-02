8962a0a54b706fb1c954795b99e762fd5f250409
Adrián Ortega <adrianog.sw@gmail.com>
2024-03-02 17:49:36 -0600

    Section 17: React Router: Building Single-Page Applications (SPA)
    Video 218: Programmatic Navigation with useNavigate

---
+++ 11-worldwise-adrian/worldwise/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
