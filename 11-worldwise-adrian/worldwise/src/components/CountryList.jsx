d31af6d9be0d9bdf5ee2853909f2dbe59f3c9a3d
Adrián Ortega <adrianog.sw@gmail.com>
2024-03-02 16:55:55 -0600

    Section 17: React Router: Building Single-Page Applications (SPA)
    Video 214: Implemeting the Countries List

---
+++ 11-worldwise-adrian/worldwise/src/components/CountryList.jsx
@@ -0,0 +1,34 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking a city on the map" />
+    );
+
+  const countries = cities.reduce((acc, city) => {
+    if (!acc.map((el) => el.country).includes(city.country))
+      return [...acc, { country: city.country, emoji: city.emoji }];
+    else return acc;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+CountryList.propTypes = {
+  cities: PropTypes.array,
+  isLoading: PropTypes.bool,
+};
+
+export default CountryList;
