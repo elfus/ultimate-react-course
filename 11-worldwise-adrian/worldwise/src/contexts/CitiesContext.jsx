3e42abd0abc223ec6950c045d75421c035dde4db
Adrián Ortega <adrianog.sw@gmail.com>
2024-03-10 12:53:55 -0600

    Section 19: Performance optimization and advanced useEffect
    Video 252: Back to the "WorldWise" App

--- 11-worldwise-adrian/worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise-adrian/worldwise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
 
 const CitiesContext = createContext();
 
@@ -74,21 +80,24 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    console.log(id, currentCity.id);
-    if (id === currentCity.id) return;
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch (error) {
-      dispatch({
-        type: "rejected",
-        payload: `Error while fetching the city: {error}`,
-      });
-    }
-  }
+  const getCity = useCallback(
+    async function getCity(id) {
+      console.log(id, currentCity.id);
+      if (id === currentCity.id) return;
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch (error) {
+        dispatch({
+          type: "rejected",
+          payload: `Error while fetching the city: {error}`,
+        });
+      }
+    },
+    [currentCity]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
