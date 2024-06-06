1a691f30beea973177d4e2a69a892f891303baef
Adrián Ortega <adrianog.sw@gmail.com>
2024-06-06 17:59:54 -0600

    Section 20: Redux and Modern Redux Toolkit
    Video 269: The Legacy Way of Connection Components to Redux

--- 15-redux-intro-adrian/src/features/accounts/BalanceDisplay.js
+++ 15-redux-intro-adrian/src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,31 @@
+import { connect } from "react-redux";
+
 function formatCurrency(value) {
+  if (!value) return 0;
+  console.log(`formatCurrency ${value}`);
   return new Intl.NumberFormat("en", {
     style: "currency",
     currency: "USD",
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+/*
+  This is the old way to connect Redux store to 
+  REACT components.
+
+  NOTE: If the connection does not work just close
+  and open the app again.
+*/
+
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(store) {
+  console.log(`mapStateToProps ${store}`);
+  return {
+    balance: store.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
