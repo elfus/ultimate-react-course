0aed40c4b7643a5461003b585054329e1f9537c4
Adrián Ortega <adrianog.sw@gmail.com>
2024-06-25 20:32:37 -0600

    Section 23: Tailwind CSS Crash Course
    Video 307: Configuring tailwind: custom font family

--- 16-fast-react-pizza-adrian/tailwind.config.js
+++ 16-fast-react-pizza-adrian/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      colors: {
+        pizza: "#123456",
+      },
+      fontSize: {
+        huge: ["80rem", { lineheight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
