bffda9b595ca0ec02f133a125ba5e6af6a2869fa
Adrián Ortega <adrianog.sw@gmail.com>
2024-07-01 20:07:01 -0600

    Section 24: Adding Redux and Advanced React
    Video 319: Deleting Cart Items

--- 16-fast-react-pizza-adrian/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza-adrian/src/features/menu/MenuItem.jsx
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types';
-import Button from '../../ui/Button';
+import DeleteItem from '../cart/DeleteItem';
 import { formatCurrency } from '../../utils/helpers';
-import { useDispatch } from 'react-redux';
-import { addItem } from '../cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import Button from '../../ui/Button';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
 
+  const isInCart = currentQuantity > 0;
   function handleAddToCart() {
     const newItem = {
       pizzaId: id,
@@ -40,7 +43,8 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {!soldOut && (
+          {isInCart && <DeleteItem pizzaId={id} />}
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
