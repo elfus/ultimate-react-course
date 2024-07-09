6393f0643dcf52c13f7cf9a43b0029ff4f0917c4
Adrián Ortega <adrianog.sw@gmail.com>
2024-07-09 11:39:39 -0600

    Section 25: Setting up our biggest project + styled components
    Video 343: Connecting supabase with our react app

---
+++ 17-the-wild-oasis-adrian/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://dleclscftllkewrzixmp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsZWNsc2NmdGxsa2V3cnppeG1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1Mzc4NTQsImV4cCI6MjAzNjExMzg1NH0.2-HZ9oR9McA3fXsKaJHNEOBf3b8l3RhbKy4t0rGL-jA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
