2d83318094234a0a9f7d60fd69a85164ec8a9d91
Adrián Ortega <adrianog.sw@gmail.com>
2024-07-11 15:25:24 -0600

    Section 27: React query: managing remote state
    Video 357: Abstracting React Query into custom hooks

--- 17-the-wild-oasis-adrian/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis-adrian/src/features/cabins/CreateCabinForm.jsx
@@ -1,16 +1,19 @@
 import { useForm } from "react-hook-form";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createEditCabin } from "../../services/apiCabins";
+import { useCreateCabin } from "./useCreateCabin";
+import { useEditCabin } from "./useEditCabin";
 
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
-import toast from "react-hot-toast";
 import FormRow from "../../ui/FormRow";
 
 function CreateCabinForm({ cabinToEdit = {} }) {
+  const { isCreating, createCabin } = useCreateCabin();
+  const { isEditing, editCabin } = useEditCabin();
+  const isWorking = isCreating || isEditing;
+
   const { id: editId, ...editValues } = cabinToEdit;
   const isEditSession = Boolean(editId);
 
@@ -19,44 +22,29 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   });
   const { errors } = formState;
 
-  const queryClient = useQueryClient();
-
-  const { mutate: createCabin, isLoading: isCreating } = useMutation({
-    mutationFn: createEditCabin,
-    onSuccess: () => {
-      toast.success("New cabin successfully created");
-      queryClient.invalidateQueries({ queryKey: ["cabins"] });
-      reset();
-    },
-    onError: (err) => {
-      toast.error(err.message);
-    },
-  });
-
-  const { mutate: editCabin, isLoading: isEditing } = useMutation({
-    mutationFn: ({ newCabinData, id }) => {
-      createEditCabin(newCabinData, id);
-    },
-    onSuccess: () => {
-      toast.success("Cabin successfully edited");
-      queryClient.invalidateQueries({ queryKey: ["cabins"] });
-      reset();
-    },
-    onError: (err) => {
-      toast.error(err.message);
-    },
-  });
-
-  const isWorking = isCreating || isEditing;
-
   function onSubmit(data) {
     const image = typeof data.image === "string" ? data.image : data.image[0];
     if (isEditSession) {
       console.log(`Calling editCabin`);
-      editCabin({ newCabinData: { ...data, image }, id: editId }, editId);
+      editCabin({ newCabinData: { ...data, image }, id: editId }, editId, {
+        onSuccess: (data) => {
+          console.log(`EDIT Success!`);
+          console.log(data);
+          reset();
+        },
+      });
     } else {
       console.log(`Calling createCabin`);
-      createCabin({ ...data, image: image });
+      createCabin(
+        { ...data, image: image },
+        {
+          onSuccess: (data) => {
+            console.log(`CREATE Success!`);
+            console.log(data);
+            reset();
+          },
+        }
+      );
     }
   }
 
