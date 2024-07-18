590aa15c95eae3edb374d0d45867bd18e838eeee
Adrián Ortega <adrianog.sw@gmail.com>
2024-07-17 18:50:57 -0600

    Section 29: Implementing more features: Authentication, Dark Mode, Dashboard
    Video 376: Client-side sorting: sorting cabins

--- 17-the-wild-oasis-adrian/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis-adrian/src/features/cabins/CabinTable.jsx
@@ -11,6 +11,8 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // 1) FILTER
+
   const filterValue = searchParams.get("discount") || "all";
   console.log(`Filter value ${filterValue}`);
 
@@ -21,6 +23,13 @@ function CabinTable() {
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -34,7 +43,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
