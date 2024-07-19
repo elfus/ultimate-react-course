63e7618d0ace356187246226b3072729bd11bc7c
Adrián Ortega <adrianog.sw@gmail.com>
2024-07-18 18:43:18 -0600

    Section 29: Implementing more features: Authentication, Dark Mode, Dashboard
    Video 380: API-Side Sorting: Sorting Bookings

--- 17-the-wild-oasis-adrian/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis-adrian/src/features/bookings/useBookings.js
@@ -11,13 +11,18 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue };
   // { field: "totalPrice", value: 5000, method: "gte" };
+
+  // SORT VALUE
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, error, bookings };
 }
