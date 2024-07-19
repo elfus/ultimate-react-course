56453f48deab4db66b5e57e81550b27a7b8d666a
Adrián Ortega <adrianog.sw@gmail.com>
2024-07-18 18:30:35 -0600

    Section 29: Implementing more features: Authentication, Dark Mode, Dashboard
    Video 379: API-Side Filtering: Filtering Bookings

--- 17-the-wild-oasis-adrian/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis-adrian/src/features/bookings/useBookings.js
@@ -1,11 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  // FILTER VALUE FROM URL
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
   const {
     isLoading,
     data: bookings,
     error,
-  } = useQuery({ queryKey: ["bookings"], queryFn: getBookings });
+  } = useQuery({
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
+  });
   return { isLoading, error, bookings };
 }
