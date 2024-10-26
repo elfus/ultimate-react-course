ba08600240d30fcaa30f65570bb4e9b065b56a45
Adrian Ortega <adrianog.sw@gmail.com>
2024-10-26 12:42:38 -0600

    Section 32: Overview of Next.js with the "App" router
    Video 437: Improving the navigation and root layout

--- 21-the-wild-oasis-website-adrian/app/layout.js
+++ 21-the-wild-oasis-website-adrian/app/layout.js
@@ -8,6 +8,7 @@ const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 console.log(josefin);
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   // title: "The Wild Oasis"
@@ -22,14 +23,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="eng">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by the The Wild Oasis</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
