887d515118316ba239c5c09e705cd7c2dd80c03e
Adrian Ortega <adrianog.sw@gmail.com>
2024-10-26 12:27:24 -0600

    Section 32: Overview of Next.js with the "App" router
    Video 435: Adding page metadata and favicon

--- 21-the-wild-oasis-website-adrian/app/layout.js
+++ 21-the-wild-oasis-website-adrian/app/layout.js
@@ -3,8 +3,15 @@ import Navigation from "@/app/_components/Navigation";
 
 import "@/app/_styles/globals.css";
 
-export const metadata = { title: "The Wild Oasis" };
-
+export const metadata = {
+  // title: "The Wild Oasis"
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="eng">
