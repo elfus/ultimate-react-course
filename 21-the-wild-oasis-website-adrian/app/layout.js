81b305ef5c5f4c90e9ae3b66da3bb2937de295be
Adrian Ortega <adrianog.sw@gmail.com>
2024-10-26 12:16:14 -0600

    Section 32: Overview of Next.js with the "App" router
    Video 424: Styling with tailwind css

--- 21-the-wild-oasis-website-adrian/app/layout.js
+++ 21-the-wild-oasis-website-adrian/app/layout.js
@@ -1,12 +1,14 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import "@/app/_styles/globals.css";
+
 export const metadata = { title: "The Wild Oasis" };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="eng">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
