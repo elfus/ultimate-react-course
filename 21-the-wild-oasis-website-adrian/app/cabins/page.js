eb2641cf27ab5b494ddc6456b50b926eaf06ee48
Adrian Ortega <adrianog.sw@gmail.com>
2024-10-29 18:21:28 -0600

    Section 32: Overview of Next.js with the "App" router
    Video 445: Fetching and siplaying cabi list

--- 21-the-wild-oasis-website-adrian/app/cabins/page.js
+++ 21-the-wild-oasis-website-adrian/app/cabins/page.js
@@ -1,13 +1,15 @@
-import Counter from "@/app/_components/Counter";
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default async function Page() {
+  console.log("Starting...");
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
+  console.log(cabins);
 
   return (
     <div>
