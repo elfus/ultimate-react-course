c6804cfef062b99072e8230a7f02f66781eaf7ee
Adrian Ortega <adrianog.sw@gmail.com>
2024-11-04 19:39:11 -0600

    Section 32: Overview of Next.js with the "App" router
    Video 465:  Sharing State Between Client and Server: The URL

--- 21-the-wild-oasis-website-adrian/app/cabins/page.js
+++ 21-the-wild-oasis-website-adrian/app/cabins/page.js
@@ -1,17 +1,25 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 // Revalidation at the page level
 // This needs to be assigned a constant value and not an expression
 // export const revalidate = 3600;
-export const revalidate = 10;
+// as long as this page is a Server Component and not a Client component
+// it will be revalidated. When is a Client component, it is no longer
+// needed.
+// export const revalidate = 10;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+// Once a Server component uses SearchParams it will become
+// a client component
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -25,8 +33,11 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
